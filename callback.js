7696b5efd0508e73aff741eeb17b3299af6b8e84
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-06-11 00:14:54 +1200

    Learned events concept, concurrency support, and how Node.js applications work

--- callback.js
+++ callback.js
@@ -2,12 +2,23 @@ var fs = require('fs');
 
 // Blocking code
 var data = fs.readFileSync('input.txt');
-console.log(data.toString());
+console.log('1: ' + data.toString());
 console.log('Blocking Program Ended');
 
 // Non-blocking code
 fs.readFile('input.txt', function(err, data) {
     if (err) return console.error(err);
-    console.log(data.toString());
+    console.log('2: ' + data.toString());
 });
 console.log('Non-Blocking Program Ended');
+
+// How Node.js apps work? - Asynchronous functions
+fs.readFile('input.txt', function(err, data) {
+    if (err) {
+        console.log(err.stack);
+        return;
+    };
+    console.log('3: ' + data.toString());
+});
+console.log('Asynchronous Functions Program Ended');
+
