f2b2c14a277213d3dbf8ceb4daa06148194e81f9
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-06-14 03:14:14 +1200

    Processing files uploaded to the server through HTML-Form's POST method with multipart/form-data encoding. Done by using the multer module in express.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express();
+const body_parser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
 
+const app = express(); // Create an express application.
 app.use(express.static('public'));
 
 // Displays index.htm when link is opened.
@@ -8,18 +11,30 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.htm');
 });
 
-// Create application/x-www-form-urlencoded parser.
-const body_parser = require('body-parser');
-const url_encoded_parser = body_parser.urlencoded({ extended : false });
+const url_encoded_parser = body_parser.urlencoded({ extended : false }); // Create application/x-www-form-urlencoded parser.
+const upload = multer({ dest : 'uploads/' }); // Middleware to handle multi-part form data.
+
 // Responds to the action event thrown by form inside index.htm.
-app.post('/process-post', url_encoded_parser, function(req, res) {
-    // Prepare output in JSON format.
-    response = {
-        first_name : req.body.first_name,
-        last_name : req.body.last_name
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+app.post('/file-upload', url_encoded_parser, upload.single('file'), function(req, res) {
+    // Debugging the various properties of a Multer file object.
+    console.log(req.file.fieldname);
+    console.log(req.file.originalname);    
+    console.log(req.file.path);
+    console.log(req.file.mimetype);
+
+    // Writing file into public/uploads/ directory.
+    const file = __dirname + '/public/uploads/' + req.file.originalname;
+    fs.readFile(req.file.path, function(err, data) {
+        fs.writeFile(file, data, function(err) {
+            if (err) { return console.error(err); }
+            response = {
+                message: 'File uploaded successfully',
+                filename: req.file.originalname
+            };
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
 });
 
 const server = app.listen(8081, '127.0.0.1', function() {
