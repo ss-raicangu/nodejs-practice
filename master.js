3244b19fdeaffc580d6986d0b9fe99f336731d00
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-06-14 19:30:50 +1200

    Using child_process.fork() to create a child process.

--- master.js
+++ master.js
@@ -2,15 +2,7 @@ const fs = require('fs');
 const child_process = require('child_process');
 
 for (let i = 0; i < 3; i++) {
-    const worker_process = child_process.spawn('node', ['support.js', i]);
-    
-    worker_process.stdout.on('data', function(data) {
-        console.log('stdout: ' + data);
-    });
-
-    worker_process.stderr.on('data', function(data) {
-        console.log('stderr: ' + data);
-    });
+    const worker_process = child_process.fork('support.js', [i]);
 
     worker_process.on('close', function(code) {
         console.log('Child process exited with exit code ' + code);
