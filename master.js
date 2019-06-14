674c0ca398ddbeac4cb047b7899b7aa06f5c5fbb
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-06-14 13:16:45 +1200

    Using child_process.spawn() to create a child process.

--- master.js
+++ master.js
@@ -2,19 +2,17 @@ const fs = require('fs');
 const child_process = require('child_process');
 
 for (let i = 0; i < 3; i++) {
-    const working_process = child_process.exec('node support.js ' + i, function(err, stdout, stderr) {
-        if (err) {
-            console.log(err.stack);
-            console.log('Error code: ' + err.code);
-            console.log('Signal received: ' + err.signal);
-        }
-        else {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-        }
-    });
+    const worker_process = child_process.spawn('node', ['support.js', i]);
     
-    working_process.on('exit', function(code) {
+    worker_process.stdout.on('data', function(data) {
+        console.log('stdout: ' + data);
+    });
+
+    worker_process.stderr.on('data', function(data) {
+        console.log('stderr: ' + data);
+    });
+
+    worker_process.on('close', function(code) {
         console.log('Child process exited with exit code ' + code);
     });
 }
