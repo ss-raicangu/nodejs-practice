b6c8503d83b50f66731f05005c2bd31a018e5aa6
ss-raicangu <49044752+ss-raicangu@users.noreply.github.com>
2019-06-14 12:20:26 +1200

    POST request to add a user to the JSON file.

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-app.get('/list-users', function(req, res) {
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.htm'));
+});
+
+const user = {
+    "user4" : {
+        "name" : "venkatesh",
+        "password" : "daggubati",
+        "profession" : "actor",
+        "id" : 4
+    }
+};
+
+app.post('/add-user', function(req, res) {
+    // First read existing users.
     fs.readFile(path.join(__dirname, 'users.json'), 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        data['user4'] = user['user4'];
         console.log(data);
-        res.end(data);
+        res.end(JSON.stringify(data));
     });
 });
 
